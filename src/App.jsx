ad219faf91b8ab65001881db13eaf10313e246d1
gyhaan <g.yhaan@alustudent.com>
2024-10-15 11:02:44 +0200

    finished landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
-import { LocationUser04Icon } from "hugeicons-react";
+import {
+  AccountSetting03Icon,
+  Agreement02Icon,
+  DiscoverCircleIcon,
+  InstagramIcon,
+  Linkedin02Icon,
+  LocationUser04Icon,
+  SafeIcon,
+  TiktokIcon,
+  WalletDone02Icon,
+  YoutubeIcon,
+} from "hugeicons-react";
 
 function App() {
   return (
@@ -51,15 +62,15 @@ function App() {
             </div>
           </div>
         </section>
-        <section className="second  bg-red-100">
+        <section className="second  ">
           <div className="wrapper">
-            <h3 className="text-2xl text-greenish font-bold leading-[1.2] text-center mb-3">
-              Experience Africa Like Never Before
+            <h3 className="max-w-96 text-2xl text-greenish font-bold leading-[1.2] text-center mb-3 mx-auto">
+              Discover the Simple Steps to Book Your Perfect African Homestay
             </h3>
             <p className=" max-w-2xl text-center mx-auto">
-              Discover the beauty of Africa through authentic homestays with
-              local hosts. Immerse yourself in vibrant cultures and create
-              unforgettable memories.
+              At Homestay Africa, we simplify booking authentic homestays,
+              allowing you to immerse in Africa's rich culture and vibrant
+              communities.
             </p>
             <div className="grid mt-7">
               <div className="flex flex-col gap-2">
@@ -68,12 +79,11 @@ function App() {
                   alt=""
                   className="rounded-lg object-cover w-full"
                 />
-                <h3 className="text-lg font-medium">Hello</h3>
+                <h3 className="text-lg font-medium">Browse Unique Stays</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nobis officia, earum porro, modi hic laudantium blanditiis rem
-                  odio eaque, placeat tempore. Eveniet quaerat vel aliquam
-                  accusamus aperiam cumque possimus deserunt?
+                  Explore a variety of homestays across Africa, from cozy
+                  village homes to urban retreats. Filter by your preferences,
+                  location, and budget.
                 </p>
               </div>
               <div className="flex flex-col gap-2">
@@ -82,12 +92,13 @@ function App() {
                   alt=""
                   className="rounded-lg object-cover w-full"
                 />
-                <h3 className="text-lg font-medium">Hello</h3>
+                <h3 className="text-lg font-medium">
+                  Choose Your Perfect Host
+                </h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nobis officia, earum porro, modi hic laudantium blanditiis rem
-                  odio eaque, placeat tempore. Eveniet quaerat vel aliquam
-                  accusamus aperiam cumque possimus deserunt?
+                  Each of our vetted hosts offer a unique experience. Read
+                  reviews, check out their profiles, and choose the one that
+                  resonates with you.
                 </p>
               </div>
               <div className="flex flex-col gap-2">
@@ -96,18 +107,17 @@ function App() {
                   alt=""
                   className="rounded-lg object-cover w-full"
                 />
-                <h3 className="text-lg font-medium">Hello</h3>
+                <h3 className="text-lg font-medium">Book with Ease</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nobis officia, earum porro, modi hic laudantium blanditiis rem
-                  odio eaque, placeat tempore. Eveniet quaerat vel aliquam
-                  accusamus aperiam cumque possimus deserunt?
+                  Once you've found your ideal stay, booking is simple and
+                  secure. Just a few clicks, and you're set for your African
+                  adventure!
                 </p>
               </div>
             </div>
           </div>
         </section>
-        <section className="second  bg-red-100">
+        <section className="second  ">
           <div className="wrapper">
             <h3 className="text-2xl text-greenish font-bold leading-[1.2] text-center mb-3">
               Benefits
@@ -118,71 +128,67 @@ function App() {
             <div className="grid mt-7">
               <div className="flex flex-col gap-2">
                 <LocationUser04Icon size={40} />
-                <h3 className="text-lg font-medium">Hello</h3>
+                <h3 className="text-lg font-medium">
+                  Authentic Cultural Experience
+                </h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nobis officia, earum porro, modi hic laudantium blanditiis rem
-                  odio eaque, placeat tempore. Eveniet quaerat vel aliquam
-                  accusamus aperiam cumque possimus deserunt?
+                  Stay with local hosts and immerse yourself in Africa's diverse
+                  cultures, traditions, and way of life.
                 </p>
               </div>
               <div className="flex flex-col gap-2">
-                <LocationUser04Icon size={40} />
-                <h3 className="text-lg font-medium">Hello</h3>
+                <Agreement02Icon size={40} />
+                <h3 className="text-lg font-medium">
+                  Support Local Communities
+                </h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nobis officia, earum porro, modi hic laudantium blanditiis rem
-                  odio eaque, placeat tempore. Eveniet quaerat vel aliquam
-                  accusamus aperiam cumque possimus deserunt?
+                  Your stay directly benefits local families and communities,
+                  promoting sustainable tourism.
                 </p>
               </div>
               <div className="flex flex-col gap-2">
-                <LocationUser04Icon size={40} />
-                <h3 className="text-lg font-medium">Hello</h3>
+                <WalletDone02Icon size={40} />
+                <h3 className="text-lg font-medium">
+                  Affordable Accommodation
+                </h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nobis officia, earum porro, modi hic laudantium blanditiis rem
-                  odio eaque, placeat tempore. Eveniet quaerat vel aliquam
-                  accusamus aperiam cumque possimus deserunt?
+                  Experience unique stays at budget-friendly prices, with
+                  options that suit all travel styles.
                 </p>
               </div>
               <div className="flex flex-col gap-2">
-                <LocationUser04Icon size={40} />
-                <h3 className="text-lg font-medium">Hello</h3>
+                <AccountSetting03Icon size={40} />
+                <h3 className="text-lg font-medium">Personalized Service</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nobis officia, earum porro, modi hic laudantium blanditiis rem
-                  odio eaque, placeat tempore. Eveniet quaerat vel aliquam
-                  accusamus aperiam cumque possimus deserunt?
+                  Enjoy the warmth of local hospitality with personalized tips,
+                  guided tours, and home-cooked meals.
                 </p>
               </div>
               <div className="flex flex-col gap-2">
-                <LocationUser04Icon size={40} />
-                <h3 className="text-lg font-medium">Hello</h3>
+                <SafeIcon size={40} />
+                <h3 className="text-lg font-medium">Safe and Secure Stays</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nobis officia, earum porro, modi hic laudantium blanditiis rem
-                  odio eaque, placeat tempore. Eveniet quaerat vel aliquam
-                  accusamus aperiam cumque possimus deserunt?
+                  All hosts are verified, providing a safe and comfortable
+                  environment for travelers.
                 </p>
               </div>
               <div className="flex flex-col gap-2">
-                <LocationUser04Icon size={40} />
-                <h3 className="text-lg font-medium">Hello</h3>
+                <DiscoverCircleIcon size={40} />
+                <h3 className="text-lg font-medium">Discover Hidden Gems</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nobis officia, earum porro, modi hic laudantium blanditiis rem
-                  odio eaque, placeat tempore. Eveniet quaerat vel aliquam
-                  accusamus aperiam cumque possimus deserunt?
+                  Explore off-the-beaten-path locations with insider knowledge
+                  from your local host.
                 </p>
               </div>
             </div>
           </div>
         </section>
         <section className="px-4">
-          <div className="img-content max-w-7xl mx-auto flex gap-1 flex-wrap  basis-0">
-            <h3>Start Your African Adventure Today</h3>
-            <div className="flex flex-col gap-3">
+          <div className="img-content max-w-7xl mx-auto flex justify-between items-center gap-x-2 gap-y-4  flex-wrap ">
+            <h3 className="text-3xl max-w-80 w-fit font-bold">
+              Start Your African Adventure Today
+            </h3>
+            <div className="flex flex-col gap-3 max-w-xl">
               <p>
                 Discover the beauty of Africa through authentic homestays that
                 connect you with local hosts. Book your unforgettable experience
@@ -192,7 +198,100 @@ function App() {
             </div>
           </div>
         </section>
+        <section className="second  ">
+          <div className="wrapper">
+            <h3 className="text-2xl text-greenish font-bold leading-[1.2] text-center mb-3">
+              FAQ
+            </h3>
+            <p className=" max-w-2xl text-center mx-auto">
+              Frequently Asked Questions
+            </p>
+            <div className="grid mt-7">
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-medium">
+                  How do I book a homestay?
+                </h3>
+                <p>
+                  You can easily book a homestay through our platform by
+                  searching for your desired location, selecting a host, and
+                  following the simple booking steps.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-medium">Are homestays safe?</h3>
+                <p>
+                  Yes! We verify all hosts to ensure a safe and secure stay for
+                  our travelers.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-medium">
+                  How do I contact my host?
+                </h3>
+                <p>
+                  Once your booking is confirmed, you will receive the host’s
+                  contact information for easy communication.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-medium">
+                  What is included in the homestay?
+                </h3>
+                <p>
+                  Each homestay varies, but many include meals, guided tours,
+                  and personalized experiences provided by your host.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-medium">
+                  How do I know where to stay?
+                </h3>
+                <p>
+                  The host is required to pick you up from an agreed upon
+                  rendezvous point.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
+      <footer>
+        <div className="wrapper">
+          <div className=" mx-auto px-10 border-2 h-60 min-h-fit border-greenish  py-7 flex flex-wrap gap-10">
+            <div>
+              <img src="/homestay-frontend.svg" alt="logo" />
+              <p className="font-bold mt-2">
+                ‘Experience Africa, Live Like a Local’
+              </p>
+            </div>
+            <nav>
+              <p className="font-bold text-greenish mb-2">Quick Links</p>
+              <ul className="flex flex-col gap-1">
+                <li>
+                  <a href="">Get Started</a>
+                </li>
+                <li>
+                  <a href="">Tours</a>
+                </li>
+                <li>
+                  <a href="">Events</a>
+                </li>
+              </ul>
+            </nav>
+            <div>
+              <p className="font-bold text-greenish mb-2">Contact</p>
+              <p>Tel: +250785685777</p>
+              <p>Email: homestay@email.io</p>
+              <div className="flex gap-4 mt-3 items-center">
+                <InstagramIcon size={32} />
+                <Linkedin02Icon size={32} />
+                <TiktokIcon size={32} />
+                <YoutubeIcon size={32} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
